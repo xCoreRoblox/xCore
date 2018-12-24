efa0f63597dbdceeaa389ffb1e7001503a11abc3
Monika <46113940+AuroraTR@users.noreply.github.com>
2018-12-24 18:17:45 -0500

    Added invite section to configuration.

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
     owner: "YourUserIDHere",
     games: {
         default: `${prefix}help | In Development`,
-        sell: `${prefix}help | Sell`
-    }
+        sell: `${prefix}help | 50% Off Premium`
+    },
+    invite: "https://discordapp.com/api/oauth2/authorize?client_id=525466950655279137&permissions=1068887494&redirect_uri=https%3A%2F%2Fxcore-roblox.tk%2Fthanks&response_type=code&scope=bot%20guilds.join"
 };
