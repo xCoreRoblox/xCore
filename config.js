d074f4411c4a974001ca0656f43683fb9e0570fd
Monika <46113940+AuroraTR@users.noreply.github.com>
2018-12-24 18:12:59 -0500

    Added base configuration which is basic.

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "YourBotTokenHere",
+    prefix: "!",
+    owner: "YourUserIDHere",
+    games: {
+        default: `${prefix}help | In Development`,
+        sell: `${prefix}help | Sell`
+    }
+};
